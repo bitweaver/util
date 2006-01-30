9bf705d853ce18a0fef1de40c41619a703d987af
Max Kremmel <xing@synapse.plus.com>
2006-01-30 10:35:32 +0000

    upgrade to latest version of GraphViz

--- GraphViz.php
+++ GraphViz.php
@@ -1,58 +1,90 @@
 <?php
 //
-// +----------------------------------------------------------------------+
-// | PEAR :: Image :: GraphViz                                            |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2002 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
-// |                    Dr. Volker GÃ¶bbels <vmg@arachnion.de>.            |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.00 of the PHP License,      |
-// | that is available at http://www.php.net/license/3_0.txt.             |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
+// +---------------------------------------------------------------------------+
+// | PEAR :: Image :: GraphViz                                                 |
+// +---------------------------------------------------------------------------+
+// | Copyright (c) 2002-2005 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
+// |                         Dr. Volker Göbbels <vmg@arachnion.de>.            |
+// +---------------------------------------------------------------------------+
+// | This source file is subject to version 3.00 of the PHP License,           |
+// | that is available at http://www.php.net/license/3_0.txt.                  |
+// | If you did not receive a copy of the PHP license and are unable to        |
+// | obtain it through the world-wide-web, please send a note to               |
+// | license@php.net so we can mail you a copy immediately.                    |
+// +---------------------------------------------------------------------------+
 //
-// $Id: GraphViz.php,v 1.1 2005/06/19 05:21:32 bitweaver Exp $
+// $Id: GraphViz.php,v 1.2 2006/01/30 10:35:32 squareing Exp $
 //
+
+require_once 'System.php';
+
 /**
-* PEAR::Image_GraphViz
-*
-* Purpose
-*
-*     Allows for the creation of and the work with directed
-*     and undirected graphs and their visualization with
-*     AT&T's GraphViz tools. These can be found at
-*     http://www.research.att.com/sw/tools/graphviz/
-*
-* Example
-*
-*     require_once 'Image/GraphViz.php';
-*     $graph = new Image_GraphViz();
-*
-*     $graph->addNode('Node1', array('URL'      => 'http://link1',
-*                                    'label'    => 'This is a label',
-*                                    'shape'    => 'box'
-*                                    )
-*                     );
-*     $graph->addNode('Node2', array('URL'      => 'http://link2',
-*                                    'fontsize' => '14'
-*                                    )
-*                     );
-*     $graph->addNode('Node3', array('URL'      => 'http://link3',
-*                                    'fontsize' => '20'
-*                                    )
-*                     );
-*
-*     $graph->addEdge(array('Node1' => 'Node2'), array('label' => 'Edge Label'));
-*     $graph->addEdge(array('Node1' => 'Node2'), array('color' => 'red'));
-*
-*     $graph->image();
-*
-* @author  Sebastian Bergmann <sb@sebastian-bergmann.de>
-*          Dr. Volker GÃ¶bbels <vmg@arachnion.de>
-* @package Image
-*/
+ * Interface to AT&T's GraphViz tools.
+ *
+ * The GraphViz class allows for the creation of and the work with directed
+ * and undirected graphs and their visualization with AT&T's GraphViz tools.
+ *
+ * <code>
+ * <?php
+ * require_once 'Image/GraphViz.php';
+ *
+ * $graph = new Image_GraphViz();
+ *
+ * $graph->addNode(
+ *   'Node1',
+ *   array(
+ *     'URL'   => 'http://link1',
+ *     'label' => 'This is a label',
+ *     'shape' => 'box'
+ *   )
+ * );
+ *
+ * $graph->addNode(
+ *   'Node2',
+ *   array(
+ *     'URL'      => 'http://link2',
+ *     'fontsize' => '14'
+ *   )
+ * );
+ *
+ * $graph->addNode(
+ *   'Node3',
+ *   array(
+ *     'URL'      => 'http://link3',
+ *     'fontsize' => '20'
+ *   )
+ * );
+ *
+ * $graph->addEdge(
+ *   array(
+ *     'Node1' => 'Node2'
+ *   ),
+ *   array(
+ *     'label' => 'Edge Label'
+ *   )
+ * );
+ *
+ * $graph->addEdge(
+ *   array(
+ *     'Node1' => 'Node2'
+ *   ),
+ *   array(
+ *     'color' => 'red'
+ *   )
+ * );
+ *
+ * $graph->image();
+ * ?>
+ * </code>
+ *
+ * @author    Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @author    Dr. Volker Göbbels <vmg@arachnion.de>
+ * @author    Karsten Dambekalns <k.dambekalns@fishfarm.de>
+ * @copyright Copyright &copy; 2002-2005 Sebastian Bergmann <sb@sebastian-bergmann.de> and Dr. Volker Göbbels <vmg@arachnion.de>
+ * @license   http://www.php.net/license/3_0.txt The PHP License, Version 3.0
+ * @category  Image
+ * @package   Image_GraphViz
+ */
 class Image_GraphViz {
     /**
     * Path to GraphViz/dot command
@@ -60,29 +92,38 @@ class Image_GraphViz {
     * @var  string
     */
     var $dotCommand = 'dot';
+
     /**
     * Path to GraphViz/neato command
     *
     * @var  string
     */
     var $neatoCommand = 'neato';
+
     /**
     * Representation of the graph
     *
     * @var  array
     */
     var $graph;
+
     /**
-    * Constructor
+    * Constructor.
     *
-    * @param  boolean Directed (true) or undirected (false) graph.
-    * @param  array   Attributes of the graph
+    * Setting the name of the Graph is useful for including multiple image maps on
+    * one page. If not set, the graph will be named 'G'.
+    *
+    * @param  boolean $directed Directed (TRUE) or undirected (FALSE) graph.
+    * @param  array   $attributes Attributes of the graph
+    * @param  string  $name Name of the Graph
     * @access public
     */
-    function Image_GraphViz($directed = true, $attributes = array()) {
+    function Image_GraphViz($directed = TRUE, $attributes = array(), $name = NULL) {
         $this->setDirected($directed);
         $this->setAttributes($attributes);
+        $this->graph['name'] = $name;
     }
+
     /**
     * Output image of the graph in a given format.
     *
@@ -90,69 +131,91 @@ class Image_GraphViz {
     *                 This may be one of the formats supported by GraphViz.
     * @access public
     */
-    function image($format = 'png') {
-        if ($file = $this->saveParsedGraph()) {
-            $outputfile = $file . '.' . $format;
-            $outputfile2 = $file . '.' . 'map';
-            $command  = $this->graph['directed'] ? $this->dotCommand : $this->neatoCommand;
-            $command .= " -T$format -o$outputfile $file";
-            @`$command`;
-            $command = $this->dotCommand;
-            $command.= " -Tcmap -o$outputfile2 $file";
-            @`$command`;
-            $fr = fopen($outputfile2,"r");
-            $map = fread($fr,filesize($outputfile2));
-            fclose($fr);
-            @unlink($file);
+    function image($format = 'svg') {
+        if ($data = $this->fetch($format)) {
+            $sendContentLengthHeader = TRUE;
+
             switch ($format) {
                 case 'gif':
-                case 'jpg':
                 case 'png':
-                case 'svg':
                 case 'wbmp': {
                     header('Content-Type: image/' . $format);
                 }
                 break;
+
+                case 'jpg': {
+                    header('Content-Type: image/jpeg');
+                }
+                break;
+
                 case 'pdf': {
                     header('Content-Type: application/pdf');
                 }
                 break;
+
+                case 'svg': {
+                    header('Content-Type: image/svg+xml');
+                }
+                break;
+
+                default: {
+                    $sendContentLengthHeader = FALSE;
+                }
             }
-            header('Content-Length: ' . filesize($outputfile));
+
+            if ($sendContentLengthHeader) {
+                header('Content-Length: ' . strlen($data));
+            }
+
+            echo $data;
+        }
+    }
+
+    /**
+    * Return image (data) of the graph in a given format.
+    *
+    * @param  string  Format of the output image.
+    *                 This may be one of the formats supported by GraphViz.
+    * @return string  The image (data) created by GraphViz.
+    * @access public
+    * @since  1.1.0
+    */
+    function fetch($format = 'svg') {
+        if ($file = $this->saveParsedGraph()) {
+            $outputfile = $file . '.' . $format;
+            $command  = $this->graph['directed'] ? $this->dotCommand : $this->neatoCommand;
+            $command .= " -T$format -o$outputfile $file";
+    
+            @`$command`;
+            @unlink($file);
+    
             $fp = fopen($outputfile, 'rb');
+    
             if ($fp) {
-                echo fread($fp, filesize($outputfile));
+                $data = fread($fp, filesize($outputfile));
                 fclose($fp);
                 @unlink($outputfile);
             }
-            @unlink($outputfile2);
-            return $map;
+    
+            return $data;
         }
+    
+        return FALSE;
     }
-    function map() {
-        if ($file = $this->saveParsedGraph()) {
-            $outputfile2 = $file . '.' . 'map';
-            $command = $this->dotCommand;
-            $command.= " -Tcmap -o$outputfile2 $file";
-            @`$command`;
-            $fr = fopen($outputfile2,"r");
-            $map = fread($fr,filesize($outputfile2));
-            fclose($fr);
-            @unlink($outputfile2);
-            @unlink($file);
-            return $map;
-        }
-    }
+
     /**
     * Add a cluster to the graph.
     *
     * @param  string  ID.
     * @param  array   Title.
+    * @param  array   Attributes of the cluster.
     * @access public
     */
-    function addCluster($id, $title) {
-        $this->graph['clusters'][$id] = $title;
+    function addCluster($id, $title, $attributes = array()) {
+        $this->graph['clusters'][$id]['title'] = $title;
+        $this->graph['clusters'][$id]['attributes'] = $attributes;
     }
+
     /**
     * Add a note to the graph.
     *
@@ -164,6 +227,7 @@ class Image_GraphViz {
     function addNode($name, $attributes = array(), $group = 'default') {
         $this->graph['nodes'][$group][$name] = $attributes;
     }
+
     /**
     * Remove a node from the graph.
     *
@@ -175,8 +239,14 @@ class Image_GraphViz {
             unset($this->graph['nodes'][$group][$name]);
         }
     }
+
     /**
     * Add an edge to the graph.
+    *
+    * Caveat! This cannot handle multiple identical edges. If you use non-numeric
+    * IDs for the nodes, this will not do (too much) harm. For numeric IDs the
+    * array_merge() that is used will change the keys when merging arrays, leading
+    * to new nodes appearing in the graph.
     *
     * @param  array Start and End node of the edge.
     * @param  array Attributes of the edge.
@@ -187,6 +257,7 @@ class Image_GraphViz {
             $from = key($edge);
             $to   = $edge[$from];
             $id   = $from . '_' . $to;
+
             if (!isset($this->graph['edges'][$id])) {
                 $this->graph['edges'][$id] = $edge;
             } else {
@@ -195,6 +266,7 @@ class Image_GraphViz {
                   $edge
                 );
             }
+
             if (is_array($attributes)) {
                 if (!isset($this->graph['edgeAttributes'][$id])) {
                     $this->graph['edgeAttributes'][$id] = $attributes;
@@ -207,6 +279,7 @@ class Image_GraphViz {
             }
         }
     }
+
     /**
     * Remove an edge from the graph.
     *
@@ -218,14 +291,17 @@ class Image_GraphViz {
               $from = key($edge);
               $to   = $edge[$from];
               $id   = $from . '_' . $to;
+
             if (isset($this->graph['edges'][$id])) {
                 unset($this->graph['edges'][$id]);
             }
+
             if (isset($this->graph['edgeAttributes'][$id])) {
                 unset($this->graph['edgeAttributes'][$id]);
             }
         }
     }
+
     /**
     * Add attributes to the graph.
     *
@@ -240,6 +316,7 @@ class Image_GraphViz {
             );
         }
     }
+
     /**
     * Set attributes of the graph.
     *
@@ -251,10 +328,11 @@ class Image_GraphViz {
             $this->graph['attributes'] = $attributes;
         }
     }
+
     /**
     * Set directed/undirected flag for the graph.
     *
-    * @param  boolean Directed (true) or undirected (false) graph.
+    * @param  boolean Directed (TRUE) or undirected (FALSE) graph.
     * @access public
     */
     function setDirected($directed) {
@@ -262,6 +340,7 @@ class Image_GraphViz {
             $this->graph['directed'] = $directed;
         }
     }
+
     /**
     * Load graph from file.
     *
@@ -273,25 +352,31 @@ class Image_GraphViz {
             $this->graph = unserialize($serialized_graph);
         }
     }
+
     /**
     * Save graph to file.
     *
     * @param  string  File to save the graph to.
-    * @return mixed   File the graph was saved to, false on failure.
+    * @return mixed   File the graph was saved to, FALSE on failure.
     * @access public
     */
     function save($file = '') {
         $serialized_graph = serialize($this->graph);
+
         if (empty($file)) {
-            $file = tempnam('temp', 'graph_');
+            $file = System::mktemp('graph_');
         }
+
         if ($fp = @fopen($file, 'w')) {
             @fputs($fp, $serialized_graph);
             @fclose($fp);
+
             return $file;
         }
-        return false;
+
+        return FALSE;
     }
+
     /**
     * Parse the graph into GraphViz markup.
     *
@@ -299,29 +384,52 @@ class Image_GraphViz {
     * @access public
     */
     function parse() {
-        $parsedGraph = "digraph G {\n";
+        if (isset($this->graph['name']) && is_string($this->graph['name'])) {
+            $parsedGraph = "digraph " . $this->graph['name'] . " {\n";
+        } else {
+            $parsedGraph = "digraph G {\n";
+        }
+
         if (isset($this->graph['attributes'])) {
             foreach ($this->graph['attributes'] as $key => $value) {
                 $attributeList[] = $key . '="' . $value . '"';
             }
+
             if (!empty($attributeList)) {
-              $parsedGraph .= implode(',', $attributeList) . ";\n";
+                $parsedGraph .= 'graph [ '.implode(',', $attributeList) . " ];\n";
             }
         }
+
         if (isset($this->graph['nodes'])) {
             foreach($this->graph['nodes'] as $group => $nodes) {
                 if ($group != 'default') {
-                  $parsedGraph .= sprintf(
-                    "subgraph \"cluster_%s\" {\nlabel=\"%s\";\n",
-                    $group,
-                    isset($this->graph['clusters'][$group]) ? $this->graph['clusters'][$group] : ''
-                  );
+                    $parsedGraph .= sprintf(
+                      "subgraph \"cluster_%s\" {\nlabel=\"%s\";\n",
+
+                      $group,
+                      isset($this->graph['clusters'][$group]) ? $this->graph['clusters'][$group]['title'] : ''
+                    );
+
+                    if (isset($this->graph['clusters'][$group]['attributes'])) {
+                        unset($attributeList);
+
+                        foreach ($this->graph['clusters'][$group]['attributes'] as $key => $value) {
+                            $attributeList[] = $key . '="' . $value . '"';
+                        }
+
+                        if (!empty($attributeList)) {
+                            $parsedGraph .= implode(',', $attributeList) . ";\n";
+                        }
+                    }
                 }
+
                 foreach($nodes as $node => $attributes) {
                     unset($attributeList);
+
                     foreach($attributes as $key => $value) {
                         $attributeList[] = $key . '="' . $value . '"';
                     }
+
                     if (!empty($attributeList)) {
                         $parsedGraph .= sprintf(
                           "\"%s\" [ %s ];\n",
@@ -330,57 +438,69 @@ class Image_GraphViz {
                         );
                     }
                 }
+
                 if ($group != 'default') {
                   $parsedGraph .= "}\n";
                 }
             }
         }
+
         if (isset($this->graph['edges'])) {
             foreach($this->graph['edges'] as $label => $node) {
                 unset($attributeList);
+
                 $from = key($node);
                 $to   = $node[$from];
+
                 foreach($this->graph['edgeAttributes'][$label] as $key => $value) {
                     $attributeList[] = $key . '="' . $value . '"';
                 }
+
                 $parsedGraph .= sprintf(
                   '"%s" -> "%s"',
                   addslashes(stripslashes($from)),
                   addslashes(stripslashes($to))
                 );
+                
                 if (!empty($attributeList)) {
                     $parsedGraph .= sprintf(
                       ' [ %s ]',
                       implode(',', $attributeList)
                     );
                 }
+
                 $parsedGraph .= ";\n";
             }
         }
+
         return $parsedGraph . "}\n";
     }
+
     /**
     * Save GraphViz markup to file.
     *
     * @param  string  File to write the GraphViz markup to.
     * @return mixed   File to which the GraphViz markup was
-    *                 written, false on failure.
+    *                 written, FALSE on failure.
     * @access public
     */
     function saveParsedGraph($file = '') {
         $parsedGraph = $this->parse();
+
         if (!empty($parsedGraph)) {
             if (empty($file)) {
-                //$file = tempnam('/tmp', 'graph_');
-                $file = TEMP_PKG_PATH.'cache/'.md5(uniqid("."));
+                $file = System::mktemp('graph_');
             }
+
             if ($fp = @fopen($file, 'w')) {
                 @fputs($fp, $parsedGraph);
                 @fclose($fp);
+
                 return $file;
             }
         }
-        return false;
+
+        return FALSE;
     }
 }
 ?>
