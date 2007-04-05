8614c654370c9a06c2befd59da3d399764e1f951
Nick Palmer <nick@sluggardy.net>
2007-04-05 20:42:51 +0000

    Add ajax_get_and_call to fetch a page and call a function with the result, caching the response for the next call.

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -8,6 +8,21 @@ function ajax_updater(target, url, data) {
 function ajax_error( request ) {
 	alert( 'Sorry, there was a problem getting the requested data.' );
 }
+function ajax_get_and_call(element, func, url, force) {
+	if (!force && element.loadedResponse) {
+		func(element.loadedResponse);
+	} else {	
+		var myAjax = new Ajax.Request(url,
+		{
+		    method:'get',
+		    onSuccess: function(transport){
+		      	element.loadedResponse = transport.responseText || "No Response.";
+			func(element.loadedResponse);
+		    },
+		    onFailure: ajax_error
+		});
+	}
+}
 function show_spinner(id) {
 	Ajax.Responders.register({
 		onCreate: function() {
