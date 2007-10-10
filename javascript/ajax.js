cb6ffd884dcabbb129bde0e33a63b82e133dfde5
wjames5 <will@tekimaki.com>
2007-10-10 18:07:19 +0000

    migrate core ajax services off prototype to mochikit

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -1,90 +1,49 @@
-var liberty_uploader_under_way = 0;
-function liberty_uploader(file, action, waitmsg, frmid) {
-	if (liberty_uploader_under_way) {
-		alert(waitmsg);
-	}
-	else {
-		liberty_uploader_under_way = 1;
-		showById('spinner');
-		var old_target = file.form.target;
-		file.form.target = frmid;
-		var old_action = file.form.action;
-		file.form.action=action;
-		file.form.submit();
-		file.form.target = old_target;
-		file.form.action = old_action;
-	}
-}
-function liberty_uploader_complete(frmid, divid, fileid) {
-	hideById('spinner');
-	var ifrm = document.getElementById(frmid);
-	if (ifrm.contentDocument) {
-		var d = ifrm.contentDocument;
-	} else if (ifrm.contentWindow) {
-		var d = ifrm.contentWindow.document;
-	} else {
-		var d = window.frames[frmid].document;
-	}
-	if (d.location.href == "about:blank") {
-		return;
-	}
-	var div = document.getElementById(divid);
-	if (div != null) {
-		div.innerHTML = d.body.innerHTML;
-		for (i=0;i<div.childNodes.length;i++) {
-			if (div.childNodes[i].id == 'result_list') {
-				div.removeChild(div.childNodes[i]);
-			}
+/* Dependencies: MochiKit Base Async */
+BitAjax = {
+	"updater": function(target, url, queryString) {
+		BitAjax.showSpinner();
+		if (queryString != "" && queryString !=null){
+			url += "?"+queryString;
 		}
-	}
-	divid = divid + '_tab';
-	div = document.getElementById(divid);
-	if (div != null) {
-		div.innerHTML = d.body.innerHTML		
-		for (i=0;i<div.childNodes.length;i++) {
-			if (div.childNodes[i].id == 'result_tab') {
-				div.removeChild(div.childNodes[i]);
-			}
+		var r = doSimpleXMLHttpRequest(url);
+		r.addCallback( BitAjax.updaterCallback, target ); 
+		r.addErrback( BitAjax.error );
+	},
+
+	"updaterCallback": function(target, rslt){
+		BitAjax.hideSpinner();
+		target.innerHTML = rslt.responseText;
+	},
+	
+	"error": function( request ) {
+		BitAjax.hideSpinner();
+		alert( 'Sorry, there was a problem getting the requested data.' );
+	},
+	
+	"getAndCall": function(elm, func, url, force) {
+		if (!force && elm.loadedResponse) {
+			func(elm.loadedResponse);
+		} else {
+			BitAjax.showSpinner();
+			var r = doSimpleXMLHttpRequest(url);
+			r.addCallback( BitAjax.getAndCallCallback, elm ); 
+			r.addErrback( BitAjax.error );
 		}
+	},
+	
+	"getAndCallCallback": function(elm, rslt){
+		BitAjax.hideSpinner();
+		elm.loadedResponse = rslt.responseText || "No Response.";
+		func(elm.loadedResponse);
+	},
+	
+	"showSpinner": function() {
+		var e = document.getElementById('spinner')
+		if (e != null){ e.style.display='block' };
+	},
+	
+	"hideSpinner": function() {
+		var e = document.getElementById('spinner')
+		if (e != null){ e.style.display='none' };
 	}
-	liberty_uploader_under_way = 0;
-	var file = document.getElementById(fileid);
-	file.value = '';
-}
-function ajax_updater(target, url, data) {
-	var myAjax = new Ajax.Updater(
-		{success: target},
-		url,
-		{parameters: data, onFailure: ajax_error}
-	);
-}
-function ajax_error( request ) {
-	alert( 'Sorry, there was a problem getting the requested data.' );
-}
-function ajax_get_and_call(element, func, url, force) {
-	if (!force && element.loadedResponse) {
-		func(element.loadedResponse);
-	} else {	
-		var myAjax = new Ajax.Request(url,
-		{
-		    method:'get',
-		    onSuccess: function(transport){
-		      	element.loadedResponse = transport.responseText || "No Response.";
-			func(element.loadedResponse);
-		    },
-		    onFailure: ajax_error
-		});
-	}
-}
-function show_spinner(id) {
-	Ajax.Responders.register({
-		onCreate: function() {
-			if (Ajax.activeRequestCount > 0)
-			Element.show(id);
-		},
-		onComplete: function() {
-			if (Ajax.activeRequestCount == 0)
-			Element.hide(id);
-		}
-	});
 }
