6bc175a7b8ef44db862faad1ecad65b30249a50f
Christian Fowler <spider@viovio.com>
2009-01-27 04:11:44 +0000

    add pidfile name to log

--- daemonize/daemonize.php
+++ daemonize/daemonize.php
@@ -68,7 +68,7 @@ if( empty( $nohup ) ) {
     if (!empty($lines[0]) && is_numeric($lines[0]) && !empty($lines[1]) && is_numeric($lines[1])) {
       # Kill hung processes.
       if ($lines[1] + 300 < time()) { // If it's an OLD pidfile...
-        to_log("OLD pidfile found from $lines[1] - ".date('d/M/Y:H:i:s O',$lines[1]).": $pid_data");
+        to_log("$pidefile OLD pidfile found from $lines[1] - ".date('d/M/Y:H:i:s O',$lines[1]).": $pid_data");
         zero_file($pidfile);
 
         # If found, kill. If it won't die, kill it harder. Then give up.
@@ -97,11 +97,11 @@ if( empty( $nohup ) ) {
 		to_log( "DAEMON $lines[0] : $daemonScript running. Last seen at ".date('d/M/Y:H:i:s O', $lines[1] ) );
         exit(0);
       } else {
-        to_log("pidfile $lines[0] found, but process is dead.");
+        to_log("$pidfile pidfile $lines[0] found, but process is dead.");
       }
     }
     else {
-      to_log("Bad format pidfile found, zeroing, restarting...");
+      to_log("$pidfile Bad format pidfile found, zeroing, restarting...");
       zero_file($pidfile);
     }
   }
