1e3294e60e8d3d0387c84bec792a8e5c98f15b1a
wjames5 <will@tekimaki.com>
2009-05-27 17:11:44 +0000

    update regex to handle mutple multi source objects

--- htmlpure/Filter/YouTube.php
+++ htmlpure/Filter/YouTube.php
@@ -8,9 +8,9 @@ class HTMLPurifier_Filter_YouTube extends HTMLPurifier_Filter
     public $name = 'YouTube';
     
     public function preFilter($html, $config, &$context) {
-        $pre_regex = '#<object.*?width="?([0-9]+)"?.*?height="?([0-9]+)"?.*?'.
+        $pre_regex = '#<object(.(?!</object))*?width="?([0-9]+)"?(.(?!</object))*?height="?([0-9]+)"?(.(?!</object))*?'.
             'http://www.youtube.com/v/([A-Za-z0-9\-_]+).+?</object>#s';
-        $pre_replace = '<span class="youtube-embed w-\1 h-\2">\3</span>';
+        $pre_replace = '<span class="youtube-embed w-\2 h-\4">\6</span>';
         $ret = preg_replace($pre_regex, $pre_replace, $html);
         return $ret;
     }
