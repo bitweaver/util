8d123bd3143d51505d2447db1bb74eb739e43f53
tekimaki <will@tekimaki.com>
2010-07-23 13:40:24 -0400

    add lib file for additional php funcs with low use and first pass of PHPAsync

---
+++ PHPAsync.php
@@ -0,0 +1,212 @@
+<?php
+/**
+
+ Usage
+ 
+ $yourObject = new YourClass();
+ $yourObject->asyncProcess = new PHPAsync();
+ $async->runProcess(
+     $yourObject,
+     'doSomeWork',
+     'getBufferContent',
+     'getLogHtml',
+ );
+
+ in YourClass::doSomeWork send notifications to the async log like so
+ 
+ $this->asyncProcess->updateStatus( <percentage_complete> );  a numeric value, do not include "%"
+
+*/
+
+require_once( UTIL_PKG_PATH.'phpcontrib_lib.php' );
+
+// written for bitweaver environment, but you can override 
+if( !defined( 'PHPASYNC_TEMP_DIR' ) ){
+	define( 'PHPASYNC_TEMP_DIR', TEMP_PKG_PATH.'phpasync' );
+}
+
+class PHPAsync extends BitBase{
+	private $mPidId;
+
+	private $mLogFile;
+
+	private $mUpdateLogHandler;
+
+	private $mFileHandle;
+
+	// default config
+	private $mConfig = array();
+
+	// constructor
+	public function __construct( $pPidId = NULL, $pConfig = array() ){
+		if( !empty( $pPidId ) ){
+			// set id
+			$this->mPidId = $pPidId;
+
+			// log file path
+			$this->mLogFile = PHPASYNC_TEMP_DIR.'/'.$this->mPidId; 
+		}
+
+		// set default config values
+		$this->mConfig = array(
+			'append_log' => FALSE,
+			'max_execution_time' => "1200",
+			'memory_limit' => '128M', 
+			'no-gzip' => 1,
+			'zlib.output_compression' => 0,
+			'ignore_user_abort' => FALSE,
+			);
+
+		// override default config
+		if( !empty( $pConfig ) ){
+			extract_to( $pConfig, $this->mConfig, EXTR_IF_EXISTS );
+		}
+
+		parent::BitBase();
+	}
+
+	// runProcess
+	/**
+	 * wraps a class instance method as a async process
+	 * @param $pObject - The object instance whose process is being wrapped
+	 * @param $pProcessHandler - The process being wrapped to run in the background
+	 * @param $pOutputHandler - The output handler is invoked to get whatever page content should be returned to the user before the background process begins
+	 * @param $pUpdateLogHandler - The update log handler is invoked when updateStatus is called by the wrapped process. the object can push customize content into the log by defining this callback
+	 */
+	public function runProcess( $pObject, $pProcessHandler, $pProcessHash = NULL, $pOutputHandler, $pUpdateLogHandler = NULL ){
+		global $gBitSystem;
+
+		// create file tracking id
+		$this->mLogFile = $this->genLogFile();
+
+		$this->mPidId = substr( $this->mLogFile, strlen(PHPASYNC_TEMP_DIR.'/') );
+
+		$this->mProcessObject = $pObject;
+
+		// register log callback
+		if( !empty( $pUpdateLogHandler ) ){
+			$this->mUpdateLogHandler = $pUpdateLogHandler;
+		}
+
+		// override some apache settings that might screw up flushing the buffer
+		@apache_setenv('no-gzip', $this->getConfig('no-gzip') );
+		@ini_set('zlib.output_compression', $this->getConfig('zlib.output_compression') );
+
+		// set time and memory
+		ini_set('max_execution_time', $this->getConfig('max_execution_time') ); 
+		ini_set('memory_limit', $this->getConfig('memory_limit') );
+
+		// allow disallow process abort
+		ignore_user_abort( $this->getConfig('ignore_user_abort') );
+
+		// create the file for writing
+		$this->mFileHandle = fopen($this->mLogFile, 'wrx+');
+		if (! $this->mFileHandle){
+			$gBitSystem->fatalError( "Error in PHPAsync: could not create log file - process aborted. Please notify your website developer." );
+		}
+
+		// output Buffered content
+		$this->outputBuffer( $pObject->$pOutputHandler() );
+
+		// run the background process
+		$pObject->$pProcessHandler( $pProcessHash );
+
+		// clean up
+		sleep(20);	// number of seconds the temp file should remain available for status check before it is deleted
+
+		fclose($this->mFileHandle);
+
+		// delete the progress file
+		unlink($this->mLogFile);
+	}
+
+	/**
+	 * this is our output buffer
+	 * send it anything and it will 
+	 * send it to the browser
+	 */
+	public function outputBuffer( $pContent ){
+		ob_start(); 
+
+		echo( 'inbuffer<br>' );
+		echo( 'pid: '.$this->mPidId.'<br>' );
+		echo( $pContent );
+		
+		$size = ob_get_length();
+		header("Content-Length: $size");
+		header('Connection: close');
+
+		ob_end_flush();
+		ob_flush();
+		flush();
+
+		session_write_close();
+	}
+
+	/**
+	 * notifies our running process 
+	 * of the percentage of a task completed
+	 */
+	public function updateStatus( $pPct ){
+		global $gBitSystem;
+
+		$logText = $pPct;
+		// if a custom log message handler is registered get the log message from it 
+		if( $func = $this->mUpdateLogHandler ){
+			$this->mProcessObject->$func( $pPct );
+		}
+		// rewind to the beginning of the log file if append is false
+		if( !$this->getConfig( 'append_log' ) ){
+			rewind( $this->mFileHandle ); 
+			ftruncate( $this->mFileHandle, filesize($$this->mLogFile));
+		}
+		// update the log file
+		if( fwrite( $this->mFileHandle, $logText ) == false ){ 
+            $gBitSystem->fatalError( "Error in PHPAsync: Write to log file failed - process aborted. " . error_get_last() );
+            exit;
+        }
+	}
+
+	/**
+	 * Retrieves the content of the log file. To be used by request checking on the status of the process
+	 */
+	public function getStatus(){ 
+		if ( file_exists( $this->mLogFile ) ){
+			if(! $progress = file_get_contents($this->mLogFile)){
+				header('HTTP/1.1 500 Internal Server Error');
+				exit;
+			}
+			return $progress;
+		}else{
+			$this->setError( 'get_status', 'log file not found' );
+			return FALSE;
+		}
+	}
+
+	public function getPidId(){
+		if( !empty( $this->mPidId ) ){
+			return $this->mPidId;
+		}
+		return NULL;
+	}
+
+	private function genLogFile(){
+        if( !is_dir( PHPASYNC_TEMP_DIR )) {
+            mkdir_p( PHPASYNC_TEMP_DIR );
+        }
+		return tempnam( PHPASYNC_TEMP_DIR, '');
+	}
+
+	private function setLogFile(){
+		if( !empty( $this->mPidId ) ){
+			$this->mLogFile = PHPASYNC_TEMP_DIR.'/'.$this->mPidId; 
+		}
+	}
+
+	private function getConfig( $pKey ){
+		if( isset( $this->mConfig[$pKey] ) ){
+			return $this->mConfig[$pKey]; 
+		}
+		return NULL;
+	}
+}
---
+++ phpcontrib_lib.php
@@ -0,0 +1,62 @@
+<?php
+// a collection of functions that are very useful
+// but not commonly needed.
+
+// extract_to
+/**
+ * @see http://www.php.net/manual/en/function.extract.php#63234
+ * 
+ * This function provides exactly the same functionality as extract except 
+ * that a parameter was added defining the extract target.
+ * This function can be used if your PHP installation does not support the 
+ * required Flags or more important if you would like to extract arrays 
+ * to another destination as to $GLOBALS, i.e. other arrays or objects.
+ * The only difference to extract is that extract_to moves the array pointer 
+ * of $arr to the end as $arr is passed by reference to support the EXTR_REFS flag.
+ */
+function extract_to( &$arr, &$to, $type=EXTR_OVERWRITE, $prefix=false ){
+   
+	if( !is_array( $arr ) ) return trigger_error("extract_to(): First argument should be an array", E_USER_WARNING );
+   
+	if( is_array( $to ) ) $t=0;
+	else if( is_object( $to ) ) $t=1;
+	else return trigger_error("extract_to(): Second argument should be an array or object", E_USER_WARNING );
+   
+	if( $type==EXTR_PREFIX_SAME || $type==EXTR_PREFIX_ALL || $type==EXTR_PREFIX_INVALID || $type==EXTR_PREFIX_IF_EXISTS )
+		if( $prefix===false ) return trigger_error("extract_to(): Prefix expected to be specified", E_USER_WARNING );
+		else $prefix .= '_';
+   
+	$i=0;
+	foreach( $arr as $key=>$val ){
+	   
+		$nkey = $key;
+		$isset = $t==1 ? isset( $to[$key] ) : isset( $to->$key );
+	   
+		if( ( $type==EXTR_SKIP && $isset )
+			|| ( $type==EXTR_IF_EXISTS && !$isset ) )
+				continue;
+	   
+		else if( ( $type==EXTR_PREFIX_SAME && $isset )
+			|| ( $type==EXTR_PREFIX_ALL )
+			|| ( $type==EXTR_PREFIX_INVALID && !preg_match( '#^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$#', $key ) ) )
+				$nkey = $prefix.$key;
+			   
+		else if( $type==EXTR_PREFIX_IF_EXISTS )
+			if( $isset ) $nkey = $prefix.$key;
+			else continue;
+
+		if( !preg_match( '#^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$#', $nkey ) ) continue;
+	   
+		if( $t==1 )
+			if( $type & EXTR_REFS ) $to->$nkey = &$arr[$key];
+			else $to->$nkey = $val;
+		else
+			if( $type & EXTR_REFS ) $to[$nkey] = &$arr[$key];
+			else $to[$nkey] = $val;
+	   
+		$i++;
+	}
+   
+	return $i;
+}
+
