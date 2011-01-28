a6bd0ecc3979d52a6549cb418f77484bd512f06c
tekimaki <will@tekimaki.com>
2011-01-28 14:50:58 -0500

    better regex

--- htmlpure/Filter/SafeIframe.php
+++ htmlpure/Filter/SafeIframe.php
@@ -5,12 +5,14 @@ class HTMLPurifier_Filter_SafeIframe extends HTMLPurifier_Filter
     public $name = 'SafeIframe';
 
     public function preFilter($html, $config, $context) {
-        return preg_replace("/iframe/", "img class=\"SafeIframe\" ", preg_replace("/<\/iframe>/", "", $html));
+		$pre_regex = '#<iframe(.+?)>#';
+		$pre_replace = '<span class="safe-iframe">\1</span>';
+		return preg_replace($pre_regex, $pre_replace, preg_replace("/<\/iframe>/", "", $html));
     }
 
     public function postFilter($html, $config, $context) {
-       $post_regex = '#<img class="SafeIframe" ([^>]+)>#';
-        return preg_replace_callback($post_regex, array($this, 'postFilterCallback'), $html);
+		$post_regex = '#<span class="safe-iframe">(.+?)</span>#';
+		return preg_replace_callback($post_regex, array($this, 'postFilterCallback'), $html);
     }
 
     protected function postFilterCallback($matches) {
