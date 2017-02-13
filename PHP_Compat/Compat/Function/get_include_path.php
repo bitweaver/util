75b7b46b9dafdee7f703d34f549941c88a9e9e15
modela bitweaver <spiderr@bitweaver.org>
2017-02-13 12:22:47 -0500

    remove-PHP_Compat

--- PHP_Compat/Compat/Function/get_include_path.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!function_exists("get_include_path")) {
-	function get_include_path() {
-		return ini_get('include_path');
-	}
-}
-?>
--- PHP_Compat/Compat/Function/set_include_path.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!function_exists("set_include_path")) {
-	function set_include_path($path) {
-		return ini_set('include_path', $path);
-	}
-}
-?>
